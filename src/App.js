1fdd36dd0b8d00d1f11fa03056fd4449b78e3e40
Victor Cordova <v.cordova@mytaxi.com>
2019-06-17 18:14:56 +0200

    Added name and age

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Header';
+import Subheader from './Subheader';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  state = {
+    name: '',
+    age: ''
+  }
+
+  nameChange = (onClickEvent) => {
+    console.log('onClickEvent: ', onClickEvent.target.value);
+    this.setState({name: onClickEvent.target.value});
+  }
+
+  ageChange = (onClickAge) => {
+    this.setState({age: onClickAge.target.value});
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Header/>
+        <Subheader/>
+        <div>
+          <label>Name:</label>
+          <input type='text' onChange={(e) => this.nameChange(e)}/>
+          <label> Age: </label>
+          <input type='number' onChange={(e) => this.ageChange(e)}/>
+        </div>
+        <div className='TemplateForBd'>
+          <p>
+            Hello: {this.state.name}, and many more happy retuns on {this.state.age}th birthday!
+          </p>
+        </div>
+        {/*
+          Add Saving information ... message. You need to create a new component (different file). In that file
+          you will need to compare old props and new props. ONLY show the message when new props are passed
+        */}
+        {/**
+          For the save the clipboard, SEARCH GOOGLE
+        */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Subheader/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => (
+    <h2>Enter the person's data</h2>
+);
